b0161eaf6200dd33dec91bc08989097a71d8b352
hustcalm <licalmer@gmail.com>
2013-12-04 14:44:45 +0800

    some refinement of exception handling and code style cleanup

--- crawJSON/douban_movie_subject.py
+++ crawJSON/douban_movie_subject.py
@@ -28,6 +28,7 @@ Pull Douban Movie Item according to the subject id
 
 import time
 import urllib,urllib.request
+from urllib.error import URLError, HTTPError
 import os
 import string
 import datetime
@@ -41,7 +42,7 @@ socket.setdefaulttimeout(timeout)
 
 #api信息
 basicUrl = 'http://api.douban.com/v2/movie/subject/'
-key='032990ce370112ae097cb26aa503380a'
+key = '032990ce370112ae097cb26aa503380a'
 
 #计数器什么的
 eid = ''
@@ -49,10 +50,10 @@ success_counter = 0
 NOTFOUNDNUM= 0
 
 #存储路径
-main_path = './jsonData'
+main_path = './newJsonData.1'
 record_path = 'record.txt'
 
-mid_avalible_txt = 'movieId-5049521--5089370.linuxmint'
+mid_avalible_txt = 'failIdList.1'
 midFile = open(mid_avalible_txt)
 lineList = midFile.readlines()
 
@@ -81,8 +82,8 @@ def download_item(loc_str,err_loc_str,eid):
         response = urllib.request.urlopen(req)
         jsonData = response.read()
 
-        #quote
-        jsonData_ = urllib.parse.quote(jsonData) #
+        # quote
+        #jsonData_ = urllib.parse.quote(jsonData)
 
         print('success:'+eid)
 
@@ -90,53 +91,63 @@ def download_item(loc_str,err_loc_str,eid):
         with open(loc_str, "wb") as json:
             json.write(jsonData)
 
-    except urllib.error.URLError as e:#有问题？？
-        #print e.code
-        print('Error!:'+str(eid))
+    except (URLError,HTTPError) as e:#有问题？？
+        print('Error for ID: '+str(eid))
+        if hasattr(e, 'code'):
+            print('Error code: ', e.code)
+        if hasattr(e, 'reason'):
+            print('Error reason: ', e.reason)
+
         with open(err_loc_str, "a") as record_file:
-            print('Record Error...')
+            print('Write Error ID to record file...')
             record_file.write(str(eid)+'\n')
         if e.code == 404:
             NOTFOUNDNUM = NOTFOUNDNUM + 1
-            print(str(NOTFOUNDNUM))
+            print('Not found number now: ' + str(NOTFOUNDNUM))
+    except socket.timeout as e:
+        print('Socket timeout for ID: ' + str(eid))
+    else:
+        pass
 
 
 
 
-while idx_last<the_file_end:
-    #存储线程,每次step个
+
+while idx_last < the_file_end:
+    # 存储线程,每次step个
     task_threads =[]
-    nextStop = min(the_file_end,idx_last+step)
-    for idx in range(idx_last,nextStop):
+    nextStop = min(the_file_end, idx_last+step)
+    for idx in range(idx_last, nextStop):
         mid_str = str(lineList[idx])
 
         print('***' + str(idx))
 
-        eid = mid_str[0:-1]#delete \n
+        eid = mid_str[0:-1] # delete \n
         print(eid)
-        loc_str = main_path+'/'+ eid +'.json'
-        err_loc_str = main_path+'/'+ 'errorList.txt'
-        t= threading.Thread( target=download_item,args=(loc_str,err_loc_str,eid) )
+
+        loc_str = main_path + '/' + eid + '.json'
+        err_loc_str = main_path + '/' + 'errorList.txt'
+        t = threading.Thread(target = download_item, args = (loc_str, err_loc_str, eid))
         task_threads.append(t)
 
 
-    #执行线程
+    # 执行线程
     numOfThreads = len(task_threads)
-    i=0
-    while i<numOfThreads:
+    i = 0
+    while i < numOfThreads:
         task = task_threads[i]
         time.sleep(1.3)
         task.start()
-        i = i+1
+        i = i + 1
 
 
     for task in task_threads:
-        #task线程合并到主线程中，主线程停止执行过程，转而执行task线程，直到task执行完毕后继续。
+        # task线程合并到主线程中，主线程停止执行过程，转而执行task线程，直到task执行完毕后继续
         task.join()
 
 
     idx_last = nextStop
-    #保存当前进度到record
+    # 保存当前进度到record
     file_loc_str = record_path
     with open(file_loc_str, "wb") as record_file:
         record_file.write(bytes(str(idx_last).encode('utf-8')))

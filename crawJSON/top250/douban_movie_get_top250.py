5ea888f1b948d7a763f0deb188e74bdf564d73f9
hustcalm <licalmer@gmail.com>
2013-12-28 19:24:50 +0800

    add Python script for crawling top250 and us_box movie JSON file

---
+++ crawJSON/top250/douban_movie_get_top250.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+Pull Douban Movie Top 250
+'''
+
+import urllib2
+import socket
+import time
+
+basicUrl = 'http://api.douban.com/v2/movie/top250'
+
+def get_items(start, count):
+    try:
+        reqUrl = basicUrl + '?start=' + str(start) + '&count=' + str(count)
+        print 'Now requesting %s ' % (reqUrl)
+        req = urllib2.Request(reqUrl)
+        response = urllib2.urlopen(req, timeout = 60)
+        #print(response.read())
+
+        # Save to file
+        print "Success pull items from " + str(start) + " to " + str(start + count - 1)
+        jsonData = response.read()
+
+        jsonFileName = 'top250-' +str(start) + '-' + str(start + count -1) + '.json'
+        print "Now saving to file: %s " % (jsonFileName)
+        with open(jsonFileName, "wb") as jsonWriter:
+            jsonWriter.write(jsonData)
+
+    except Exception as e:
+        print 'Exception: %s' % (e)
+
+print 'Start Work...'
+# Get to work
+get_items(0, 100)
+get_items(100, 100)
+get_items(200, 50)
+print 'Done!'
